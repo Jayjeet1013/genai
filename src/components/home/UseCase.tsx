8ca47defb68ccb9097035997b572911de646154c
Jayjeet1013 <jayjeet1013@gmail.com>
2023-08-07 11:52:22 +0530

    complete usecase part

---
+++ src/components/home/UseCase.tsx
@@ -0,0 +1,126 @@
+import { Box, Container, Flex, Text,Link } from '@chakra-ui/react'
+import React from 'react'
+import { HiOutlineArrowUpRight } from "react-icons/hi2";
+
+const UseCase = () => {
+  return (
+   <Box bg={'#1a1d24'} pb={32}>
+        <Container maxW={1300}>
+        <Box textColor={'white'}>
+                <Box textAlign={'center'} p={8}>
+                        <Text py={8} textColor={'yellow'}>GenAI Use Cases</Text>
+                        <Text fontWeight={'bold'} fontSize={{base:'24px',md:'40px'}} px={48}>Write Better Content Faster, The Future Of AI Writing Tools is Here
+</Text>
+                </Box>
+
+                <Flex  direction={{base:'column',md:'row'}} justifyContent={'space-between'}>
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        
+                        
+
+                </Flex>
+                <Flex  direction={{base:'column',md:'row'}} justifyContent={'space-between'}>
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        <Box p={4}>
+                        <Text py={4} fontWeight={'bold'} >Writing Blog Content</Text>
+                        <Text pb={4}>Writing blog content with GenAI, make sure you have a clear understanding of who your audience is.</Text>
+                        <Link href="#" display={'flex'}  pt={4}  _hover={{ textDecoration: 'underline',fontWeight:'bold' }}>
+                <Text  textColor={"yellow"}>
+                  Get Started Free 
+                </Text>  <HiOutlineArrowUpRight
+            style={{ fontSize: 16, color: "yellow",marginLeft:5,marginTop:4}}
+          />
+              </Link>
+                        </Box>
+                        
+                        
+                        
+
+                </Flex>
+        </Box>
+        </Container>
+   </Box>
+  )
+}
+
+export default UseCase
