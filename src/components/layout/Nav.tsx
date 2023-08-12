c3781152c379291295ae5ccbe255d15926dce00b
Jayjeet1013 <jayjeet1013@gmail.com>
2023-08-13 01:51:30 +0530

    menu done

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,9 +1,19 @@
 import { Box, Container, Text ,Flex,Link,Button,Image} from '@chakra-ui/react'
 import React from 'react'
+import { AiOutlineMenu} from "react-icons/ai";
+import {AiOutlineClose} from "react-icons/ai"
+import { useState } from 'react';
 
 
 
 const Nav = () => {
+
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  }
+
   return (
     <Box bg={'#1a1d24'}  position="fixed" zIndex={100} width="100%">
    <Container maxW={1300}>
@@ -40,8 +50,26 @@ const Nav = () => {
     <Button p={6} color='yellow'  _hover={{bgColor:"yellow",color:'black'}} border={'2px solid yellow'}  bgColor={'transparent'}>Get Started</Button>
   </Box>
  </Flex>
+
+ <Box display={{ base: "flex", md: "none" }}  onClick={toggleMenu}>
+            {menu ? <AiOutlineClose style={{ color: "yellow", fontSize: 32 }} /> : <AiOutlineMenu style={{ color: "yellow", fontSize: 32 }} />}
+          </Box>
     </Flex>
    </Container>
+   <Box w={"full"}  bg={'#1a1d24'} px={8} pb={6} pt={2} display={{ base: menu ? "block" : "none", md: "none" }}>
+   <Flex color={'white'} gap={4}    justifyContent="start" direction={'column'} display={{ base: "flex", md: "none" }}>
+  <Link _hover={{color:'yellow',textDecoration:'none'}} > <Text >Home</Text></Link>
+  <Link _hover={{color:'yellow',textDecoration:'none'}}  ><Text >Pages</Text></Link> 
+  <Link _hover={{color:'yellow',textDecoration:'none'}}  ><Text >Use cases</Text></Link>
+  <Link _hover={{color:'yellow',textDecoration:'none'}}   ><Text >Pricing</Text></Link>
+  <Link _hover={{color:'yellow',textDecoration:'none'}}  ><Text>Contact</Text></Link>
+  <Link _hover={{color:'yellow',textDecoration:'none'}}><Text>Login</Text></Link>
+
+  <Box color={'yellow.800'}>
+    <Button p={6} color='yellow'  _hover={{bgColor:"yellow",color:'black'}} border={'2px solid yellow'}  bgColor={'transparent'}>Get Started</Button>
+  </Box>
+ </Flex>
+      </Box>
    </Box>
   )
 }
